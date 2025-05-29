c4a069ea32ddcc205989ebf673fe869c8db7db75
Matt06743 <matt06743@gmail.com>
2025-05-29 14:48:30 +0100

    complete create_sql & where_clause with tests

--- python/src/extract/helper_create_sql.py
+++ python/src/extract/helper_create_sql.py
@@ -3,16 +3,26 @@ import datetime
 
 #time utc
 
-def create_sql(table:str,columns:list,from_time:datetime,to_time:datetime) -> str:
-    """_summary_
+def create_sql(table:str, from_time:datetime, to_time:datetime) -> str:
+    """produces a sql string that selects all columns from a given table with a 
+    time from and to from a given time
 
     Args:
-        table (str): _description_
-        columns (list): _description_
-        from_time (datetime): _description_
-        to_time (datetime): _description_
+        table (str): table extracting from
+        from_time (datetime): data from this time
+        to_time (datetime): data to this time
 
     Returns:
-        str: _description_
+        str: the sql query
     """    
-    return "nothing"
+    where_clause = add_where_clause(from_time, to_time)
+    return f"SELECT * FROM {identifier(table)} {where_clause};"
+
+def add_where_clause(from_time:datetime, to_time:datetime)-> str:
+    """creates the where clause
+
+    """
+    str_from_time = from_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    str_to_time = to_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    print(literal(from_time), str_to_time)
+    return f"WHERE last_updated BETWEEN {literal(str_from_time)} and {literal(str_to_time)}"
--- python/tests/test_extract/test_helper_create_sql.py
+++ python/tests/test_extract/test_helper_create_sql.py
@@ -1,13 +1,64 @@
-from python.src.extract.helper_create_sql import create_sql
+from python.src.extract.helper_create_sql import create_sql, add_where_clause
 from datetime import datetime,timedelta,timezone
+import pytest
+
+@pytest.fixture(scope='module')
+def from_time(to_time):
+    return to_time - timedelta(minutes=15) 
+
+@pytest.fixture(scope='module')
+def to_time():
+    return datetime(2025, 5, 29, 12, 30, 00, 1000)
+
 
 class TestCreate_sql:
-    def test_create_sql_returns_string(self):
+    def test_create_sql_returns_string(self, from_time, to_time):
         table = 'test'
-        columns=['column1']
-        from_time = datetime.now(timezone.utc) - timedelta(minutes=15) 
-        to_time = datetime.now(timezone.utc)
-
-        returned = create_sql(table,columns,from_time,to_time)
-
+        returned = create_sql(table, from_time, to_time)
         assert isinstance(returned,str)
+
+    def test_string_begins_with_select_all_from(self, from_time, to_time):
+        table = 'test'
+        start = f"SELECT * FROM {table}"
+        returned = create_sql(table, from_time, to_time)
+        assert returned.startswith(start)
+
+    def test_protect_table_name_from_injection(self, from_time, to_time):
+        table = 'test;'
+        start = f'SELECT * FROM "{table}"'
+        returned = create_sql(table, from_time, to_time)
+        assert returned.startswith(start)
+
+    def test_query_ends_with_semicolon(self, from_time, to_time):
+        table = 'test;'
+        returned = create_sql(table, from_time, to_time)
+        assert returned.endswith(';')
+
+    def test_query_contains_where_clause(self, from_time, to_time):
+        table = 'test;'
+        returned = create_sql(table, from_time, to_time)
+        assert 'WHERE' in returned
+
+    def test_query_has_where_clause(self, from_time, to_time):
+        #arrage - combined select and where clause
+        table = 'test'
+        start = f'SELECT * FROM {table} '
+        add_where = add_where_clause(from_time, to_time)
+        expected = start + add_where + ';' 
+        #act
+        returned = create_sql(table, from_time, to_time)
+        #assert
+        assert returned == expected
+
+class TestAddWhereClause:
+    def test_add_where_clause(self, from_time, to_time):
+        returned = add_where_clause(from_time, to_time)
+        print(from_time, to_time, 'test')
+        #2025-05-29 12:30:00.001
+        assert isinstance(returned,str)
+
+    def test_where_clause_correct(self, from_time, to_time):
+        returned = add_where_clause(from_time, to_time)
+        expected = "WHERE last_updated BETWEEN '2025-05-29 12:15:00.001' and '2025-05-29 12:30:00.001'"
+        assert returned == expected
+
