ed0e0b176957ea30d1e8c701087e1ce2e64204e2
Zakii <zakiii1995@outlook.com>
2025-05-29 11:33:16 +0100

    Create variable, zip bucket and create versioning

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,11 +7,26 @@ resource "aws_s3_bucket" "raw_data_bucket" {
     Environment = "Dev"
   }
 }
-
 resource "aws_s3_bucket_versioning" "TT_raw_data_versioning" {
   # Enable versioning for the TT Terraform state bucket
   bucket = aws_s3_bucket.raw_data_bucket.id
 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "zip_bucket" {
+  bucket = var.zip_bucket
+  force_destroy = true
+  tags = {
+    Name = "tt_zip_bucket"
+    Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket_versioning" "tt_zip_bucket_versioning" {
+  # Enable versioning for the TT Terraform state bucket
+  bucket = aws_s3_bucket.zip_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
