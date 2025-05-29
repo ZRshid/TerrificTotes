8490602f0ff8c643466f28829fe39bbf912cd741
Xin Miao <197311566+xinmiao14@users.noreply.github.com>
2025-05-29 10:06:33 +0100

    Add Terraform Initial Set Up
    * Add versioning annd force destory to backend bucket. Create data.tf vars.tf s3.tf main.tf iam.tf files
    
    * Add IAM policies for lambda and S3
    
    * add cloudwatch policy and assign policy to the lambda role
    
    ---------
    
    Co-authored-by: Camilla82 <camillabertini@gmail.com>

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,3 +3,15 @@ variable "raw_data_bucket" {
   type        = string
   default     = "tt-raw-data"
 }
+
+variable "backend_bucket" {
+  description = "the backend bucket: TerrificTotes-tfstate"
+  type        = string
+  default     = "snacks-tt-tfstate"
+}
+
+variable "lambda_name" {
+  description = "lambda handler name"
+  type        = string
+  default     = "lambda_template"
+}
