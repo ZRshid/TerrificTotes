a2388c0ae55e10d02d92ef9b0ad922faa60e7787
Camilla82 <camillabertini@gmail.com>
2025-05-30 15:53:18 +0100

    add tests for get_secret function with error

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -1,9 +1,11 @@
 from python.utils.utils import get_secret
 import os  
 import boto3
+from botocore.exceptions import ClientError
 from moto import mock_aws 
 import pytest 
 import json 
+
 
 @pytest.fixture()
 def aws_credentials():
@@ -15,17 +17,17 @@ def aws_credentials():
     os.environ['AWS_SESSION_TOKEN'] = 'test'
     os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
-@pytest.fixture
+@pytest.fixture()
 def mock_secrets_manager(aws_credentials): # check this: why aws_credentials is not accessed.
     with mock_aws():
         client = boto3.client('secretsmanager')
         yield client
-                               
-class TestGetSecret:
 
+
+class TestGetSecret:
+    
     def test_get_secret_returns_expected_secret(self, mock_secrets_manager):
         secret_name = "test_secret"
-
         expected_secret = {
             'username': 'test_user',
             'password': 'fake_password',
@@ -33,34 +35,20 @@ class TestGetSecret:
             'host': 'localhost',
             'port': 5432
         }
-    
+
+        response = mock_secrets_manager.create_secret(Name = secret_name, SecretString = json.dumps(expected_secret))
+
         result = get_secret(secret_name)
 
         assert result == expected_secret
     
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
-
-    # def test_creates_secret(self,mock_secrets_manager):
-    #     expected_name = "My_test_secret"
-    #     expected_secret = '{"user_id": "my_user", "password": "my_password"}'
-
-
-
-    #     results = store_secret(mock_secrets_manager,
-    #                           user_id="my_user",
-    #                           password="my_password",
-    #                           name=expected_name)
-    #     response = mock_secrets_manager.get_secret_value(SecretId=expected_name)
-    #     pprint(response)
-    #     assert response['SecretString'] == expected_secret
-    #     assert response["Name"] == expected_name
-    #     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
-
-
-
-
-
+    def test_wrong_secrets(self, mock_secrets_manager):
+        missing_secret_name = "nonexistent_secret"
+        with pytest.raises(ClientError) as err:
+            mock_secrets_manager.get_secret_value(SecretId=missing_secret_name)
 
 
 
--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,21 +1,29 @@
 import boto3
 from botocore.exceptions import ClientError
-from pprint import pprint
+
 import json
 import logging 
 
 
-
 def get_secret(secrets_name):
+
+    """
+    Get a secret from AWS Secrets Manager.
+    Args:
+        secret_name (str): The name of the secret you want to get.
+    Returns:
+        dict: The secret data as a dictionary.
+        None: If there was an error getting the secret.
+    """
+
     secrets_manager = boto3.client('secretsmanager')
     try:
         response=secrets_manager.get_secret_value(SecretId=secrets_name)
         secret_dict = json.loads(response['SecretString'])
+        logging.info["Secrets retrieved"]
         return secret_dict
     except ClientError as e:
         logging.error(f"The following error has occurred {e}") # logging error - does it go to Cloudwatch and trigger error?
-
-pprint(get_secret("totesys_secret")) 
 
 
 
