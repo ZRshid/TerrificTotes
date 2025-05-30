edced7ebe11f9914e8fce5b4a094795688dadbf9
Matt06743 <matt06743@gmail.com>
2025-05-30 16:03:41 +0100

    add comment to extract handler py

--- python/src/extract/extract_handler.py
+++ python/src/extract/extract_handler.py
@@ -1,5 +1,7 @@
 import logging
 
+#this can set any level of that the message actually appears 
+#otherwise its default is none which doesnt log it
 logging.getLogger().setLevel(logging.INFO)
 
 def lambda_handler(event, context):
