b4e072c61b19c5dafd56fab1aa26b461695e56ea
Xin Miao <197311566+xinmiao14@users.noreply.github.com>
2025-05-31 23:00:03 +0100

    Update logging and raise error

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,12 +1,9 @@
 import boto3
 from botocore.exceptions import ClientError
-
 import json
 import logging 
 
-
 def get_secret(secrets_name):
-
     """
     Get a secret from AWS Secrets Manager.
     Args:
@@ -15,18 +12,12 @@ def get_secret(secrets_name):
         dict: The secret data as a dictionary.
         None: If there was an error getting the secret.
     """
-
     secrets_manager = boto3.client('secretsmanager')
     try:
         response=secrets_manager.get_secret_value(SecretId=secrets_name)
         secret_dict = json.loads(response['SecretString'])
-        logging.info["Secrets retrieved"]
+        logging.info("Secrets retrieved")
         return secret_dict
     except ClientError as e:
         logging.error(f"The following error has occurred {e}") # logging error - does it go to Cloudwatch and trigger error?
-
-
-
-
-
-
+        raise e
