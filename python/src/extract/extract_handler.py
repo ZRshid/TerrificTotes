756cf17267a16c9b57a66f0a369d262f72db83e2
Zakii <zakiii1995@outlook.com>
2025-06-03 00:58:00 +0100

    Resolve merge conflict and update CloudWatch alarm configuration

--- python/src/extract/extract_handler.py
+++ python/src/extract/extract_handler.py
@@ -1,13 +1,39 @@
 import logging
+from datetime import datetime
 
-#this can set any level of that the message actually appears 
-#otherwise its default is none which doesnt log it
-logging.getLogger().setLevel(logging.INFO)
+
+logger = logging.getLogger() #This line creates a tool to write messages about what our program is doing
+logger.setLevel(logging.INFO) #This line tells that tool to only pay attention to important messages and above
 
 def lambda_handler(event, context):
-    logging.info(msg='hello')
-    print('hi')
-    raise Exception('intentional error')
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+    try:
+        if "tables" in event and len(event["tables"]) > 0: 
+            logger.info(f"extract succeeded: {len(event['tables'])} tables found at {formatted_time}")
+            return {
+                "status": "success",
+                "message": f"Extracted {len(event['tables'])} tables.",
+                "tables": event["tables"]
+            }
+        else:
+            raise ValueError("Missing or empty tables list") #Signals an error when something is missing such as tables etc.     
+    except Exception as err:
+        logger.error(f"Extraction has failed {err}")
+        return {
+              "status": "error",
+                "message": f"Extract failed: {err}"
+            }
+    
+if __name__ == "__main__":
+    test_event = {"tables": ["table_name","sales"],
+                  "from_time" : "2025-06-02 11:30:55.00",
+                  "to_time" : "2025-06-02 11:31:55.00",
+                  "raw_data_bucket" : "tt-raw-data"}           
+    result = lambda_handler(test_event, None)
+    print(result)
+
+
 
 
 
--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,88 +1,23 @@
-# #create a cloudwatch metric alarm for specified resources
+#create a cloudwatch metric alarm for specified resources
 resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
   alarm_name          = "lambda-${aws_lambda_function.extract_handler.function_name}-error-alarm"
   alarm_description   = "triggers when the Lambda has 1 or more errors in 30 secs"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = var.evaluation_periods
   metric_name         = "Errors"
   namespace           = "AWS/Lambda"
-  period              = 30
+  period              = var.period
   statistic           = "Sum"
-  threshold           = 1
+  threshold           = var.threshold
   treat_missing_data  = "notBreaching"
-  alarm_actions = [ aws_sns_topic.lambda_alerts.arn ]
+  alarm_actions = [aws_sns_topic.lambda_alerts.arn ]
 
   #to filter/identify a specific resource
   dimensions = {
     FunctionName = aws_lambda_function.extract_handler.function_name
   }
+
 }
-
-#create alarm, triggers alarm when lambda handler is successful once 
-# resource "aws_cloudwatch_metric_alarm" "lambda_success_alarm" {
-#   alarm_name          = "lambda-${aws_lambda_function.extract_handler.function_name}-success-alarm"
-#   alarm_description   = "triggers when the Lambda has 1 or more success in 30 secs"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = 1
-#   metric_name         = "SuccessfulExecutions"
-#   namespace           = "test/Lambda"
-#   period              = 30
-#   statistic           = "Sum"
-#   threshold           = 1
-#   treat_missing_data  = "notBreaching"
-#   alarm_actions = [ aws_sns_topic.lambda_alerts.arn ]
-
-#   #to filter/identify a specific resource
-#   dimensions = {
-#     FunctionName = aws_lambda_function.extract_handler.function_name
-#   }
-# }
-
-#fancy alarm that just subtracts invocations to errors 
-# resource "aws_cloudwatch_metric_alarm" "lambda_success_alarm" {
-#   alarm_name          = "lambda-${aws_lambda_function.extract_handler.function_name}-success-alarm"
-#   alarm_description   = "Triggers if there are fewer than 1 successful Lambda executions in 3 min"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = 1
-#   threshold           = 1
-#   treat_missing_data  = "notBreaching"
-#   alarm_actions       = [aws_sns_topic.lambda_alerts.arn]
-
-#   metric_query {
-#     id = "invocations"
-#     metric {
-#       metric_name = "Invocations"
-#       namespace   = "AWS/Lambda"
-#       period      = 10
-#       stat        = "Sum"
-#       dimensions = {
-#         FunctionName = aws_lambda_function.extract_handler.function_name
-#       }
-#     }
-#     return_data = true
-#   }
-
-#   metric_query {
-#     id = "errors"
-#     metric {
-#       metric_name = "Errors"
-#       namespace   = "AWS/Lambda"
-#       period      = 10
-#       stat        = "Sum"
-#       dimensions = {
-#         FunctionName = aws_lambda_function.extract_handler.function_name
-#       }
-#     }
-#     return_data = false
-#   }
-
-#   metric_query {
-#     id          = "successes"
-#     expression  = "invocations - errors"
-#     label       = "Successful Invocations"
-#     return_data = true
-#   }
-# }
 
 resource "aws_sns_topic" "lambda_alerts" {
   name = "lambda-error-alerts"
