fc6863b10862abcedb5eb0db32f1cb9c064c12ee
Camilla82 <camillabertini@gmail.com>
2025-06-04 11:48:56 +0100

    modify load_currency.py

--- python/src/transform/load_currency.py
+++ python/src/transform/load_currency.py
@@ -1,6 +1,10 @@
 import pandas as pd 
+import awswrangler as wr
 
-
+def load_currency_codes_from_s3(columns:list[str], path=['s3://zip-bucket/currency-codes.csv']):
+    currency_df = wr.s3.read_csv(path, usecols=columns)
+    currency_df['currency_id'] = currency_df.index  
+    return currency_df
 
 def load_currency_codes(file:str, columns:list[str]) -> pd.DataFrame:
      
@@ -9,3 +13,6 @@ def load_currency_codes(file:str, columns:list[str]) -> pd.DataFrame:
     currency_df['currency_id'] = currency_df.index     
     print(currency_df)
     return currency_df
+
+
+    
