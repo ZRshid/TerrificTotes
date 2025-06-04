725f13345fee3c849d9313d5be3e3f5fcb5b7a6e
Mark <64991712+m-pr@users.noreply.github.com>
2025-06-04 15:38:19 +0100

    refactor load_json so that it converts the json to a dataframe correctly, and drop the from_time, to_time fields, plus write tests

--- python/src/transform/load_json.py
+++ python/src/transform/load_json.py
@@ -1,12 +1,39 @@
 import awswrangler as wr
 import pandas as pd
 import logging
+import boto3
+import json
 
-def load_json(bucket: str, key: str) -> pd.DataFrame:
+
+def load_json(bucket: str, key: str,table:str,s3) -> pd.DataFrame:
+    """Load a JSON file from s3.
+
+    The file must contain a first level key equal to table.
+
+
+    Args:
+        bucket (str): The s3 bucket.
+        key (str): The files key, including path.
+        table (str): The table it is loading from.
+        s3: A boto3 3s client.
+
+    Raises:
+        e: ClientErrors and others
+
+    Returns:
+        pd.DataFrame: a DataFrame with all the columns from the tables JSON.
+    """    
+    
     try:
-        path = f"s3://{bucket}/{key}"
-        result = wr.s3.read_json([path])
-        return result
+        # path = f"s3://{bucket}/{key}"
+        # result = wr.s3.read_json([path],boto3_session= boto3_session)
+        # return result
+        object = s3.get_object(Bucket = bucket,Key = key)
+        content= object.get('Body').read().decode('utf-8')
+        data = json.loads(content)
+        
+        df=pd.DataFrame(data[table])
+        return df
     except Exception as e:
-        logging.error(msg=f"load json fail {e} in bucket: {bucket} and key: {key}")
+        logging.error(msg=f"Load_json failed: '{e}'. For bucket: {bucket} and key: {key}")
         raise e
--- python/tests/test_transform/test_load_json.py
+++ python/tests/test_transform/test_load_json.py
@@ -1,13 +1,86 @@
-from src.transform.load_json import load_json
+import json
+import os
+import boto3.session
+import botocore
+import botocore.errorfactory
 import pytest
-from unittest.mock import patch
 import pandas as pd
-import awswrangler as wr
+# import awswrangler as wr
+import boto3
+from moto import mock_aws
+from src.transform.load_json import load_json
+from unittest.mock import patch
+
+TEST_BUCKET = 'test-bucket'
+TEST_TABLE = 'address'
+TEST_JSON = """{
+    "from_time": "2022-01-02 23:30:00.00", 
+    "to_time": "2025-06-02 23:59:59.00", 
+    "address": [
+        {"address_id": 1, "address_line_1": "6826 Herzog Via", "address_line_2": null, "district": "Avon", "city": "New Patienceburgh", "postal_code": "28441", "country": "Turkey", "phone": "1803 637401", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"},
+        {"address_id": 2, "address_line_1": "179 Alexie Cliffs", "address_line_2": null, "district": null, "city": "Aliso Viejo", "postal_code": "99305-7380", "country": "San Marino", "phone": "9621 880720", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}
+    ]
+}"""
+TEST_KEY = "test/test.json"
+
+
+@pytest.fixture()
+def s3():
+    with mock_aws():
+        yield boto3.client('s3', region_name='eu-west-2')
+
+@pytest.fixture(autouse=True)
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'test'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'test'
+    os.environ['AWS_SECURITY_TOKEN'] = 'test'
+    os.environ['AWS_SESSION_TOKEN'] = 'test'
+    os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
+
+@pytest.fixture()
+def s3_with_bucket(s3):
+    
+    s3.create_bucket(
+        Bucket=TEST_BUCKET,
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'}
+    )
+    s3.put_object(
+            Bucket = TEST_BUCKET,
+            Body = TEST_JSON,
+            Key = TEST_KEY
+    )
+    return s3
 
 class TestLoadJson:
-    @patch("src.transform.load_json.wr")
-    def test_returns_dataframe(self, wr):
-        expected = pd.DataFrame({'key': [1, 2], 'key2': [2, 3]})
-        wr.s3.read_json.return_value=expected
-        result = load_json('bucket', 'key')
-        assert isinstance(result, pd.DataFrame)
+    # @patch("src.transform.load_json.wr")
+    # def test_returns_dataframe(self, wr):
+    #     expected = pd.DataFrame({'key': [1, 2], 'key2': [2, 3]})
+    #     wr.s3.read_json.return_value=expected
+    #     result = load_json('bucket', 'key')
+    #     assert isinstance(result, pd.DataFrame)
+
+    def test_returns_dataframe(self,s3_with_bucket):
+        s3 = s3_with_bucket
+        result_df = load_json(TEST_BUCKET,TEST_KEY,TEST_TABLE,s3)
+        assert isinstance(result_df, pd.DataFrame)
+
+    def test_returns(self,s3_with_bucket):
+        expected_columns = ["address_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]
+        result_df = load_json(TEST_BUCKET,TEST_KEY,TEST_TABLE,s3_with_bucket)
+        result_columns = result_df.columns.to_list()
+        for expected in expected_columns:
+            assert expected in result_columns
+
+    def test_does_not_return_date_columns(self,s3_with_bucket):
+        unwanted_columns = ['from_time','to_time']
+        result_df = load_json(TEST_BUCKET,TEST_KEY,TEST_TABLE,s3_with_bucket)
+        result_columns = result_df.columns.to_list()
+        for expected in unwanted_columns:
+            assert expected not in result_columns
+
+    def test_raises_correctly(self,s3):
+        #e.g no bucket
+        with pytest.raises(botocore.exceptions.ClientError) as e:
+            result_df = load_json(TEST_BUCKET,TEST_KEY,TEST_TABLE,s3)
+        
