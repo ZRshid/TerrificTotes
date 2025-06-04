508552942ba0455031e8fb2d6078bb0f414cd439
Camilla82 <camillabertini@gmail.com>
2025-06-04 15:44:41 +0100

    add test load_currency_codes_from_s3

--- python/tests/test_transform/test_load_currency.py
+++ python/tests/test_transform/test_load_currency.py
@@ -1,6 +1,8 @@
-from src.transform.transform_currency import load_currency_codes
+from python.src.transform.load_currency import load_currency_codes, load_currency_codes_from_s3
 import pandas as pd  
 import pytest 
+from unittest.mock import patch
+import awswrangler as wr
 
 class Test_Load_Currency_Codes():
     
@@ -15,7 +17,7 @@ class Test_Load_Currency_Codes():
         df = pd.DataFrame 
         file = "../Data/Currency-codes.csv"
         headers_csv = ["currency_code", "currency_name"]
-        result_df = load_currency_codes(file, headers_csv) # df
+        result_df = load_currency_codes(file, headers_csv)
         columns = result_df.columns.to_list()
         
         assert "currency_code" in columns
@@ -30,5 +32,31 @@ class Test_Load_Currency_Codes():
 
         assert "currency_id" in columns 
 
+    # patching s3.read_csv so that I can mock my data and feed them 
+    #into my load codes from s3
+    @patch("awswrangler.s3.read_csv")
+    def test_func_loads_codes_from_s3(self, mock_read_csv):
+      
+        mock_read_csv.return_value = pd.DataFrame({
+            "currency_code": ["USD", "EUR"],
+            "currency_name": ["US Dollar", "Euro"]
+        })
+
+        # Act
+        result = load_currency_codes_from_s3(columns=["currency_code", "currency_name"])
+
+        # Assert
+        #returns df
+        assert isinstance(result, pd.DataFrame)
+        #is the column in the df
+        assert "currency_id" in result.columns
+        # checking dimensions
+        assert result.shape[0] == 2 #pandas.DataFrame.shape 
+        # Return a tuple representing the dimensionality of the DataFrame.
 
 
+
+
+
+
+            
