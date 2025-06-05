a5dfc69635ac3eae13712c191e7725498edc6086
Matt06743 <matt06743@gmail.com>
2025-06-05 11:38:19 +0100

    add logging & some error handling

--- python/src/transform/transform_counterparty.py
+++ python/src/transform/transform_counterparty.py
@@ -1,46 +1,71 @@
 import pandas as pd
+import logging
 
 
 def transform_counterparty(
     counterparty: pd.DataFrame, address: pd.DataFrame
 ) -> pd.DataFrame:
-    df_drop_address = address.drop(
-        ["created_at", "last_updated", "from_time", "to_time"],
-        axis="columns",
-        errors="ignore",
-    )
+    """create dim_counterparty by merging counterparty 
+    and address dataframe with address_id and then dropping/renaming columns
 
-    df_merged = pd.merge(
-        counterparty,
-        df_drop_address,
-        how="left",
-        left_on="legal_address_id",
-        right_on="address_id",
-    )
+    Args:
+        counterparty (pd.DataFrame): original counterparty dataframe
+        address (pd.DataFrame): the address dataframe
 
-    df_merged_drop = df_merged.drop(
-        [
-            "created_at",
-            "last_updated",
-            "commercial_contact",
-            "delivery_contact",
-            "address_id",
-            "legal_address_id",
-        ],
-        axis="columns",
-        errors="ignore",
-    )
+    Returns:
+        pd.DataFrame: a dataframe with columns: {
+                "counterparty_id",
+                "counterparty_legal_name",
+                "counterparty_legal_address_line_1",
+                "counterparty_legal_address_line_2",
+                "counterparty_legal_district",
+                "counterparty_legal_city",
+                "counterparty_legal_postal_code",
+                "counterparty_legal_country",
+                "counterparty_legal_phone_number",
+            }
+    """
+    try:
+        df_drop_address = address.drop(
+            ["created_at", "last_updated", "from_time", "to_time"],
+            axis="columns",
+            errors="ignore",
+        )
 
-    df_renamed_merged = df_merged_drop.rename(
-        columns={
-            "address_line_1": "counterparty_legal_address_line_1",
-            "address_line_2": "counterparty_legal_address_line_2",
-            "district": "counterparty_legal_district",
-            "city": "counterparty_legal_city",
-            "postal_code": "counterparty_legal_postal_code",
-            "country": "counterparty_legal_country",
-            "phone": "counterparty_legal_phone_number",
-        }
-    )
-    
-    return df_renamed_merged
+        df_merged = pd.merge(
+            counterparty,
+            df_drop_address,
+            how="left",
+            left_on="legal_address_id",
+            right_on="address_id",
+        )
+
+        df_merged_drop = df_merged.drop(
+            [
+                "created_at",
+                "last_updated",
+                "commercial_contact",
+                "delivery_contact",
+                "address_id",
+                "legal_address_id",
+            ],
+            axis="columns",
+            errors="ignore",
+        )
+
+        df_renamed_merged = df_merged_drop.rename(
+            columns={
+                "address_line_1": "counterparty_legal_address_line_1",
+                "address_line_2": "counterparty_legal_address_line_2",
+                "district": "counterparty_legal_district",
+                "city": "counterparty_legal_city",
+                "postal_code": "counterparty_legal_postal_code",
+                "country": "counterparty_legal_country",
+                "phone": "counterparty_legal_phone_number",
+            }
+        )
+
+        return df_renamed_merged
+    except Exception as e:
+        logging.error(msg=f"failed to create dim_counterparty dataframe due to error: {e}")
+        raise e
