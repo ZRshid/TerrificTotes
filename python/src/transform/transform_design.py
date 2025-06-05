ae75bc1df80b23d9042e52bd529a0135ae9e4d07
Zakii <zakiii1995@outlook.com>
2025-06-05 15:25:16 +0100

    Add solutions and tests for the design table

---
+++ python/src/transform/transform_design.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def transform_design(design:pd.DataFrame):
+    
+    """
+    Saves the transformed DataFrame in its correct format to match the project star schema
+    Args:
+        design (pd.DataFame): The name of the table being transformed    
+    Returns:
+        The transformed table 
+    Raises:
+        Exception: If there is an error during the S3 upload.
+    """
+    try:
+        df_updated_design_table = design.drop(columns=["created_at", "last_updated"] , axis =1)
+        df_ascending_order = df_updated_design_table.sort_values(by="design_id", ascending=True)
+        df_set_index = df_ascending_order.set_index("design_id")
+        return df_set_index
+    except Exception as e:
+        raise e 
+
+
+
