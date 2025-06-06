629f6e60df61a763c9515aaee1927d1caaaf36d7
Camilla Bertini <126681504+Camilla82@users.noreply.github.com>
2025-06-06 10:00:47 +0100

    Update transform_payment_type.py

--- python/src/transform/transform_payment_type.py
+++ python/src/transform/transform_payment_type.py
@@ -6,18 +6,15 @@ def transform_payment_type(payment_type: pd.DataFrame) -> pd.DataFrame:
     which drops non_essential metadata columns
     Args:
         payment_type (pd.DataFrame): the original dataframe
-
-    Raises:
-        e: catches any error not caught
-
+    
     Returns:
-        pd.DataFrame: a dim_payment_types dataframe
+        pd.DataFrame: a dim_payment_types dataframe, returns consists of two columns: payment_type_id and payment_type_name 
     """
     try:
-        df_dropped = payment_type.drop(["created_at", "last_updated"],
+        df_dim_payment_type = payment_type.drop(["created_at", "last_updated"],
                                         axis="columns",
                                         errors="ignore")
-        return df_dropped
+        return df_dim_payment_type
     except Exception as e:
         logging.error(msg=f"fail to create dim_payment_type due to error: {e}")
         raise e
