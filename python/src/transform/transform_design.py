61e3f66a5f497e1f25682dcbe2869eb77dd2a2de
Xin Miao <197311566+xinmiao14@users.noreply.github.com>
2025-06-06 09:31:22 +0100

    Update transform_design.py

--- python/src/transform/transform_design.py
+++ python/src/transform/transform_design.py
@@ -16,7 +16,8 @@ def transform_design(design:pd.DataFrame):
         df_updated_design_table = design.drop(columns=["created_at", "last_updated"] , axis =1)
         df_ascending_order = df_updated_design_table.sort_values(by="design_id", ascending=True)
         # df_set_index = df_ascending_order.set_index("design_id")
-        return df_set_index
+        # return df_set_index
+        return df_ascending_order
     except Exception as e:
         logging.error(msg=f"Failed to make the design datafram: {e}")
         raise e
