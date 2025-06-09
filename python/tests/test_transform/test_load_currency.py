daf6c1d70478c018d0fad832a8546742fef1e640
Mark <64991712+m-pr@users.noreply.github.com>
2025-06-09 18:44:14 +0100

    create sanitize_currency and test

--- python/tests/test_transform/test_load_currency.py
+++ python/tests/test_transform/test_load_currency.py
@@ -1,10 +1,45 @@
-from src.transform.load_currency import load_currency_codes, load_currency_codes_from_s3
-import pandas as pd  
+from src.transform.load_currency import load_currency_codes, load_currency_codes_from_s3, sanitize_codes
+import pandas as pd
 import pytest 
 from unittest.mock import patch
 import awswrangler as wr
 import os
+from io import StringIO
 
+CURRENCY_CSV ="""
+country,currency_name,currency_code
+AFGHANISTAN,Afghani,AFN
+ÅLAND ISLANDS,Euro,EUR
+ALBANIA,Lek,ALL
+ALGERIA,Algerian Dinar,DZD
+AMERICAN SAMOA,US Dollar,USD
+ANDORRA,Euro,EUR
+ANGOLA,Kwanza,AOA
+ANGUILLA,East Caribbean Dollar,XCD
+ANTARCTICA,No universal currency,
+ANTIGUA AND BARBUDA,East Caribbean Dollar,XCD
+ARAB MONETARY FUND,Arab Accounting Dinar,XAD
+ARGENTINA,Argentine Peso,ARS
+ARMENIA,Armenian Dram,AMD
+ARUBA,Aruban Florin,AWG
+AUSTRALIA,Australian Dollar,AUD
+AUSTRIA,Euro,EUR
+"""
+AFTER_CSV ="""
+country,currency_name,currency_code
+AFGHANISTAN,Afghani,AFN
+ÅLAND ISLANDS,Euro,EUR
+ALBANIA,Lek,ALL
+ALGERIA,Algerian Dinar,DZD
+AMERICAN SAMOA,US Dollar,USD
+ANGOLA,Kwanza,AOA
+ANGUILLA,East Caribbean Dollar,XCD
+ARAB MONETARY FUND,Arab Accounting Dinar,XAD
+ARGENTINA,Argentine Peso,ARS
+ARMENIA,Armenian Dram,AMD
+ARUBA,Aruban Florin,AWG
+AUSTRALIA,Australian Dollar,AUD
+"""
 @pytest.fixture
 def filepath():
     cwd = os.getcwd()
@@ -12,7 +47,16 @@ def filepath():
         cwd = cwd[:-7]
     return cwd+"/Data/Currency-codes.csv"
 
-class Test_Load_Currency_Codes():
+@pytest.fixture
+def currency_df():
+    currency_df= pd.read_csv(StringIO(CURRENCY_CSV), usecols=["currency_code", "currency_name"])
+    return currency_df
+@pytest.fixture
+def expected_df():
+    currency_df= pd.read_csv(StringIO(AFTER_CSV), usecols=["currency_code", "currency_name"])
+    return currency_df
+
+class Test_Load_currency_codes():
     
     def test_func_return_df(self,filepath):
         df = pd.DataFrame
@@ -31,9 +75,12 @@ class Test_Load_Currency_Codes():
         
         assert "currency_code" in columns
         assert "currency_name" in columns
-        assert "currency_id" in columns 
+        assert "currency_id" in columns
+    def test_currency_id_is_sequential(self,filepath ):
+        result = load_currency_codes(filepath,columns=["currency_code", "currency_name"])
+        assert (result["currency_id"].diff()[1:]==1).all()
         
-
+class TestLoad_currency_codes_from_s3():
     @patch("awswrangler.s3.read_csv")
     def test_func_loads_codes_from_s3(self, mock_read_csv):
       
@@ -49,7 +96,26 @@ class Test_Load_Currency_Codes():
        
         assert "currency_id" in result.columns
         
-        assert result.shape[0] == 2 
+        assert result.shape[0] == 2
+    @patch("awswrangler.s3.read_csv")
+    def test_currency_id_is_sequential(self, mock_read_csv,currency_df ):
+        mock_read_csv.return_value = currency_df
+        result = load_currency_codes_from_s3(columns=["currency_code", "currency_name"])
+        assert (result["currency_id"].diff()[1:]==1).all()
+
+
+    class TestSanitize_codes():
+        def test_returns_df(self,currency_df):
+            result = sanitize_codes(currency_df)
+            assert isinstance(result, pd.DataFrame)
+            assert result is not currency_df
+        def test_has_no_nulls(self,currency_df):
+            result = sanitize_codes(currency_df)
+            assert result[["currency_code", "currency_name"]].notnull().values.all()
+        def test_returns_correct_df(self,currency_df,expected_df):
+            result = sanitize_codes(currency_df)
+            pd.testing.assert_frame_equal(result,expected_df)
+            
      
 
 
