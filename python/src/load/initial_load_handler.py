11efe58525215b2becb2d41806c3eb87a3bfd5d6
Mark <64991712+m-pr@users.noreply.github.com>
2025-06-10 16:59:41 +0100

    change function name to load_to_db to match new name

--- python/src/load/initial_load_handler.py
+++ python/src/load/initial_load_handler.py
@@ -2,7 +2,7 @@ import logging
 from src.load.load import download_parquet_from_s3_and_saves_it_in_memory, convert_buffer_to_dataframe
 from src.transform.initial_transform_handler import make_key
 from utils.utils import get_secret
-from unittest.mock import Mock as create_sql
+from src.load.create_sql import load_to_db
 
 #event = {"transformed_tables", "timestamp": }
 logging.getLogger().setLevel(logging.INFO)
@@ -33,7 +33,7 @@ def insert_parquet_file_to_db(timestamp, secrets, table_name):
     buffer = download_parquet_from_s3_and_saves_it_in_memory(BUCKET,key)
     df_table = convert_buffer_to_dataframe(buffer)
 
-    inserted_rows = create_sql(df_table,table_name, secrets['username'],secrets['password'],secrets['host'],secrets['dbname'])
+    inserted_rows = load_to_db(df_table,table_name, secrets['username'],secrets['password'],secrets['host'],secrets['dbname'])
     return inserted_rows
 
 
