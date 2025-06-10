247a182b9869e92806f7f9c915397e728276ca6e
Camilla Bertini <126681504+Camilla82@users.noreply.github.com>
2025-06-10 14:16:39 +0100

    Update test_load.py
    Take out secret manager class from test file.

--- python/tests/test_load/test_load.py
+++ python/tests/test_load/test_load.py
@@ -20,11 +20,11 @@ def aws_credentials():
     os.environ['AWS_SESSION_TOKEN'] = 'test'
     os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
-@pytest.fixture()
-def mock_secrets_manager(aws_credentials): 
-    with mock_aws():
-        client = boto3.client('secretsmanager')
-        yield client
+# @pytest.fixture()
+# def mock_secrets_manager(aws_credentials): 
+#     with mock_aws():
+#         client = boto3.client('secretsmanager')
+#         yield client
 
 @pytest.fixture()
 def mock_s3_boto():
@@ -42,28 +42,28 @@ def mock_s3_bucket(mock_s3_boto):
      mock_s3_boto.put_object(Bucket=bucket, Key=key, Body=body)
      return bucket, key, body
 
-class TestGetSecret:
-    def test_get_secret_returns_expected_secret(self, mock_secrets_manager):
-        secret_name = "test_datawarehouse"
-        expected_secret = {
-            'username': 'test_user',
-            'password': 'fake_password',
-            'engine': 'postgres',   
-            'host': 'localhost',
-            'port': 5432
-        }
-        response = mock_secrets_manager.create_secret(
-            Name = secret_name, 
-            SecretString = json.dumps(expected_secret)
-            )
-        result = get_secret(secret_name)
-        assert result == expected_secret
-        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+# class TestGetSecret:
+#     def test_get_secret_returns_expected_secret(self, mock_secrets_manager):
+#         secret_name = "test_datawarehouse"
+#         expected_secret = {
+#             'username': 'test_user',
+#             'password': 'fake_password',
+#             'engine': 'postgres',   
+#             'host': 'localhost',
+#             'port': 5432
+#         }
+#         response = mock_secrets_manager.create_secret(
+#             Name = secret_name, 
+#             SecretString = json.dumps(expected_secret)
+#             )
+#         result = get_secret(secret_name)
+#         assert result == expected_secret
+#         assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
-    def test_get_secret_raises_an_error_when_secret_does_not_exist(self):
-        missing_secret_name = "nonexistent_secret"
-        with pytest.raises(ClientError):
-            get_secret(missing_secret_name)
+    # def test_get_secret_raises_an_error_when_secret_does_not_exist(self):
+    #     missing_secret_name = "nonexistent_secret"
+    #     with pytest.raises(ClientError):
+    #         get_secret(missing_secret_name)
 
 
 class TestDownloadingParquetFromS3():
