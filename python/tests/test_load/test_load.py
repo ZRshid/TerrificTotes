6713009dbfd933cc01e5809bf5cf92c0be963df1
Mark <64991712+m-pr@users.noreply.github.com>
2025-06-11 10:19:46 +0100

    remove import get_secret

--- python/tests/test_load/test_load.py
+++ python/tests/test_load/test_load.py
@@ -1,4 +1,4 @@
-from src.load.load import get_secret, download_parquet_from_s3_and_saves_it_in_memory, convert_buffer_to_dataframe
+from src.load.load import download_parquet_from_s3_and_saves_it_in_memory, convert_buffer_to_dataframe
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pandas as pd
@@ -20,12 +20,6 @@ def aws_credentials():
     os.environ['AWS_SESSION_TOKEN'] = 'test'
     os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
-# @pytest.fixture()
-# def mock_secrets_manager(aws_credentials): 
-#     with mock_aws():
-#         client = boto3.client('secretsmanager')
-#         yield client
-
 @pytest.fixture()
 def mock_s3_boto():
     """Create an S3 boto3 client and return the client object"""
@@ -41,30 +35,6 @@ def mock_s3_bucket(mock_s3_boto):
      mock_s3_boto.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': 'eu-west-2'})
      mock_s3_boto.put_object(Bucket=bucket, Key=key, Body=body)
      return bucket, key, body
-
-# class TestGetSecret:
-#     def test_get_secret_returns_expected_secret(self, mock_secrets_manager):
-#         secret_name = "test_datawarehouse"
-#         expected_secret = {
-#             'username': 'test_user',
-#             'password': 'fake_password',
-#             'engine': 'postgres',   
-#             'host': 'localhost',
-#             'port': 5432
-#         }
-#         response = mock_secrets_manager.create_secret(
-#             Name = secret_name, 
-#             SecretString = json.dumps(expected_secret)
-#             )
-#         result = get_secret(secret_name)
-#         assert result == expected_secret
-#         assert response['ResponseMetadata']['HTTPStatusCode'] == 200
-
-    # def test_get_secret_raises_an_error_when_secret_does_not_exist(self):
-    #     missing_secret_name = "nonexistent_secret"
-    #     with pytest.raises(ClientError):
-    #         get_secret(missing_secret_name)
-
 
 class TestDownloadingParquetFromS3():
     def test_downloads_parquest_file_from_an_s3_bucket_into_IoBytes(self, mock_s3_boto,mock_s3_bucket):
